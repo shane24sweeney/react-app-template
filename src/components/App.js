68bf54534417a72de044f0d91ce0fc07a97355df
Shane Sweeney <shane24sweeney@gmail.com>
2022-11-18 08:48:42 -0500

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,24 @@
 import React, { Component } from 'react';
-import Projects from '../components/Projects';
-import SocialProfiles from '../components/SocialProfiles';
-import profile from '../assets/profile.png';
-import Title from '../components/Title';
 import '../index';
 
 class App extends Component {
 
-    state = {displayBio: false};
-
-    toggleDisplayBio = () => {
-        this.setState({displayBio: !this.state.displayBio})
-    }
-
+   
     render(){
         return(
         <div>
-            <img src={profile} alt='profile' className='profile'/>
-            <h1>Name</h1>
-            <p>My name is Shane</p>
-            <Title/> 
             
-            {
-                 this.state.displayBio ?(
-                     <div>
-                     <p>I live in Ballinora</p>
-                     <p>I have loads of buds</p>
-                     <button onClick={this.toggleDisplayBio}>Show less</button>
+            React App
+        
         </div>
-        )
-         : (
-            <div>
-                <button onClick={this.toggleDisplayBio}>Read More </button>
-            </div>
-        )
-         }
-         <hr/>
-         <Projects/>
-         <hr/>
-         <SocialProfiles/>
-      
-    </div>)
-         
-    }
+        );
+       
+    
+        }      
+    
 }
 
 
 
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import Header from './components/Header';
 import App from './components/App';
-import Jokes from './components/Jokes';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<BrowserRouter history={createBrowserHistory()}>
-<Routes>
-  <Route path = '/' element = {<Header><App /></Header>} />
-  <Route path = '/jokes' element = {<Header> <Jokes /></Header>} />
-</Routes>
-
-</BrowserRouter>
+root.render(<App/>
 );
 
 /*new Promise((resolve, reject) =>  {
